1cc3f4df74448b1e53f2f431e685b0eb88caa7a1
blackCauldron7 <blackcauldron7@protonmail.com>
2021-08-17 00:48:20 +0530

    fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,41 +4,41 @@
 </script>
 
 <script>
+  import { invoke } from '../../node_modules/@tauri-apps/api/tauri';
+  import { listen } from '../../node_modules/@tauri-apps/api/event';
   import { mode, games, page, query, selectedGenre } from '$lib/store.js';
+  import Sidebar from '$lib/Sidebar.svelte';
   import GameGrid from '$lib/GameGrid.svelte';
   import Pagination from '$lib/Pagination.svelte';
   import GameList from '$lib/GameList.svelte';
-  import { onMount } from 'svelte';
 
-  let opts = { page_number: $page - 1, page_size: 20 };
+  $: {
+    let opts = { page_number: $page - 1, page_size: 20 };
 
-  if ($query != '') {
-    opts.search = $query;
-  }
-  if ($selectedGenre != '') {
-    opts.filter_genre = $selectedGenre;
-  }
+    if ($query != '') {
+      opts.search = $query;
+    }
+    if ($selectedGenre != '') {
+      opts.filter_genre = $selectedGenre;
+    }
 
-  onMount(async () => {
-    const { listen } = await import('@tauri-apps/api/event');
-    const { invoke } = await import('@tauri-apps/api/tauri');
     invoke('get_games', { opts })
       .then((g) => ($games = g))
       .catch((err) => console.error(err));
+  }
 
-    invoke('get_local_games')
-      .then(console.log)
-      .catch((err) => console.error(err));
+  invoke('get_local_games')
+    .then(console.log)
+    .catch((err) => console.error(err));
 
-    invoke('run_game', { index: 7 })
-      .then(console.log)
-      .catch((err) => console.error(err));
+  invoke('run_game', { index: 7 })
+    .then(console.log)
+    .catch((err) => console.error(err));
 
-    listen('game_log', (event) => {
-      // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
-      // event.payload is the payload object
-      console.log(event.payload);
-    });
+  listen('game_log', (event) => {
+    // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+    // event.payload is the payload object
+    console.log(event.payload);
   });
 </script>
 
