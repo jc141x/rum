2820b0bfab99f753d9b86952ee6a167564f25cb9
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-08-17 16:39:02 +0200

    Tried to improve description truncating

--- src/lib/GameCard.svelte
+++ src/lib/GameCard.svelte
@@ -1,3 +1,12 @@
+<style>
+    .description {
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+</style>
+
 <script>
   import { selectedGame } from '$lib/store.js';
   import { invoke } from '../../node_modules/@tauri-apps/api/tauri';
@@ -8,13 +17,6 @@
     return game.banner_path === null
       ? banner
       : `https://gitlab.com/chad-productions/chad_launcher_banners/-/raw/master/${game.banner_path}`;
-  }
-  function truncateString(str, n) {
-    if (str.length > n) {
-      return str.substring(0, n) + '...';
-    } else {
-      return str;
-    }
   }
   function toTitleCase(str) {
     return str.replace(/\w\S*/g, function (txt) {
@@ -27,10 +29,10 @@
   };
 </script>
 
-<div class="card p-0 flex-fill w-200 m-10">
+<div class="card p-0 flex-fill w-250 m-10 d-flex flex-column">
   <!-- Image -->
-  <img class="img-fluid rounded-top" src={get_banner(game)} alt={game.name} />
-  <div class="content m-15 overflow-y-scroll h-200">
+  <img class="img-fluid rounded-top w-full mh-full" src={get_banner(game)} alt={game.name} />
+  <div class="content m-15 h-250 overflow-hidden mh-full d-flex flex-column">
     <!-- Title -->
     <header class="font-size-18 font-weight-bold">
       {game.name}
@@ -39,18 +41,20 @@
       {toTitleCase(game.type)}
     </p>
     <!-- Genres -->
+    <div class="">
     {#each game.genres as genre}
       <span class="badge"> {genre} </span>
     {/each}
+    </div>
     {#if game.nsfw}
       <span class="badge badge-danger"> 18+ </span>
     {/if}
     <!-- Description -->
-    <p class="">
+    <p class="description flex-shrink-1 overflow-hidden">
       {game.description}
     </p>
     <!-- Download -->
-    <div class="text-right">
+    <div class="text-right mt-auto">
       <button on:click={() => handleMagnet()} class="btn" target="_blank">Download</button>
     </div>
   </div>
