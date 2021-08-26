8ecf1e3910010455f5e0a580195b3ba75045f66d
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-08-26 10:18:17 +0200

    Sidebar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,42 +1,30 @@
 <script>
-    import "roboto-fontface-woff/css/roboto/sass/roboto-fontface.scss";
-    import "../global.scss";
+  import 'roboto-fontface-woff/css/roboto/sass/roboto-fontface.scss';
+  import '../global.scss';
+
+  import Navbar from '$lib/Navbar.svelte';
+  import Sidebar from '$lib/Sidebar.svelte';
   import Header from '$lib/Header.svelte';
   import Index from './index.svelte';
   import Library from './library.svelte';
   import Discover from './discover.svelte';
   import Documentation from './documentation.svelte';
   import { MaterialApp, Button, Window, WindowItem } from 'svelte-materialify/src';
-import Settings from "./settings.svelte";
+  import Settings from './settings.svelte';
   let theme = 'dark';
   let value = 0;
 </script>
 
 <MaterialApp {theme}>
-  <Header bind:value />
-  <Window {value} class="pa-4">
-    <WindowItem>
-      <Index />
-    </WindowItem>
-    <WindowItem>
-      <Library />
-    </WindowItem>
-    <WindowItem>
-      <Discover />
-    </WindowItem>
-    <WindowItem>
-      <Documentation />
-    </WindowItem>
-    <WindowItem>
-      <Settings />
-    </WindowItem>
-  </Window>
+  <Navbar />
+  <Sidebar />
+  <slot />
 </MaterialApp>
 
 <style lang="scss">
   @import 'svelte-materialify/src/styles/variables';
 
   :global(body) {
-    background-color: map-get($material-dark-theme, "surface");
+    background-color: map-get($material-dark-theme, 'surface');
   }
 </style>
