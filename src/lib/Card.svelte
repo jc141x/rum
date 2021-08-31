85efaea8f2d5951f29a9fa9b22d315c00a24f6c5
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-08-31 18:12:05 +0200

    Fix issue where missing banners are too high

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -9,14 +9,22 @@
 
 <div on:click>
   <Card class={color}>
-    <img src={banner} alt="banner" />
+    <div class="img-container">
+      <img src={banner} alt="banner" />
+    </div>
     <CardTitle>{title}</CardTitle>
   </Card>
 </div>
 
 <style>
-  img {
+  .img-container {
+    position: relative;
     width: 100%;
-    height: 100%;
+    padding-top: 46.74%;
+  }
+  img {
+    max-width: 100%;
+    position: absolute;
+    top: 0;
   }
 </style>
