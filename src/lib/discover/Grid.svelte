774eca9e07026555bc25f981ce9e037cbb742378
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-08-31 17:54:13 +0200

    Fix weird bug

--- src/lib/discover/Grid.svelte
+++ src/lib/discover/Grid.svelte
@@ -18,25 +18,6 @@
 {:catch error}
   <p style="color: red">{error.message}</p>
 {/await}
-<div class="full">
-  {#await $databaseGames}
-    <div class="d-flex justify-center align-center full">
-      <ProgressCircular indeterminate color="primary" />
-    </div>
-  {:then games}
-    <Grid>
-      {#each games as game, i (game.id)}
-        <Card
-          {game}
-          selected={$selectedGame == i}
-          on:click={() => selectedGame.set(i)}
-        />
-      {/each}
-    </Grid>
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-</div>
 
 <style>
   .full {
