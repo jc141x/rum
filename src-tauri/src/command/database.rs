9c325017f249d27806ea23d68bbc662370756e14
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-09-02 13:59:49 +0200

    Database administration commands

--- src-tauri/src/command/database.rs
+++ src-tauri/src/command/database.rs
@@ -1,6 +1,7 @@
 use super::TauriChadError;
 use chad_rs::database::{self, table, DatabaseFetcher, GetGamesOpts};
 use serde::de::DeserializeOwned;
+use serde::Serialize;
 
 #[tauri::command]
 pub async fn database_get_games(
@@ -41,4 +42,102 @@ pub async fn database_get_tags(
     fetcher: tauri::State<'_, DatabaseFetcher>,
 ) -> Result<Vec<String>, TauriChadError> {
     get_items::<table::ListTags>(&*fetcher).await
+}
+
+#[tauri::command]
+pub async fn database_add_update_game(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game: database::Game,
+) -> Result<(), TauriChadError> {
+    fetcher
+        .add_update_game(&game.game, &game.languages, &game.genres, &game.tags)
+        .await
+        .map_err(|e| TauriChadError::from(e))
+}
+
+#[tauri::command]
+pub async fn database_remove_game(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+) -> Result<(), TauriChadError> {
+    fetcher
+        .remove_game(&game_id)
+        .await
+        .map_err(|e| TauriChadError::from(e))
+}
+
+async fn add_items<T: table::Item + Serialize>(
+    fetcher: &DatabaseFetcher,
+    game_id: &database::table::GameId,
+    items: &[String],
+) -> Result<(), TauriChadError> {
+    fetcher
+        .add_items::<T>(game_id, items)
+        .await
+        .map_err(|e| TauriChadError::from(e))
+}
+
+#[tauri::command]
+pub async fn database_add_genres(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    genres: Vec<String>,
+) -> Result<(), TauriChadError> {
+    add_items::<table::Genre>(&fetcher, &game_id, &genres).await
+}
+
+#[tauri::command]
+pub async fn database_add_languages(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    languages: Vec<String>,
+) -> Result<(), TauriChadError> {
+    add_items::<table::Language>(&fetcher, &game_id, &languages).await
+}
+
+#[tauri::command]
+pub async fn database_add_tags(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    tags: Vec<String>,
+) -> Result<(), TauriChadError> {
+    add_items::<table::Genre>(&fetcher, &game_id, &tags).await
+}
+
+async fn delete_items<T: table::Item + Serialize>(
+    fetcher: &DatabaseFetcher,
+    game_id: &database::table::GameId,
+    items: &[String],
+) -> Result<(), TauriChadError> {
+    fetcher
+        .delete_items::<T>(game_id, items)
+        .await
+        .map_err(|e| TauriChadError::from(e))
+}
+
+#[tauri::command]
+pub async fn database_delete_genres(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    genres: Vec<String>,
+) -> Result<(), TauriChadError> {
+    delete_items::<table::Genre>(&fetcher, &game_id, &genres).await
+}
+
+#[tauri::command]
+pub async fn database_delete_languages(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    languages: Vec<String>,
+) -> Result<(), TauriChadError> {
+    delete_items::<table::Language>(&fetcher, &game_id, &languages).await
+}
+
+#[tauri::command]
+pub async fn database_delete_tags(
+    fetcher: tauri::State<'_, DatabaseFetcher>,
+    game_id: database::table::GameId,
+    tags: Vec<String>,
+) -> Result<(), TauriChadError> {
+    delete_items::<table::Genre>(&fetcher, &game_id, &tags).await
 }
