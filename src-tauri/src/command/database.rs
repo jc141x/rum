24d68b79a1644f1509d5c874ed9b44b418b0a013
Gnurur GNUrrrrrrr <gnurur@protonmail.com>
2021-09-09 11:17:40 +0200

    Use DatabaseFetcher::list_items

--- src-tauri/src/command/database.rs
+++ src-tauri/src/command/database.rs
@@ -1,6 +1,5 @@
 use super::TauriChadError;
 use chad_rs::database::{self, table, DatabaseFetcher, GetGamesOpts};
-use serde::de::DeserializeOwned;
 
 #[tauri::command]
 pub async fn database_get_games(
@@ -10,35 +9,32 @@ pub async fn database_get_games(
     fetcher.get_games(&opts).await.map_err(|e| e.into())
 }
 
-async fn get_items<T: table::Table + Into<String> + DeserializeOwned>(
-    fetcher: &DatabaseFetcher,
-) -> Result<Vec<String>, TauriChadError> {
-    Ok(fetcher
-        .list_table::<T>()
-        .await
-        .map_err(|e| TauriChadError::from(e))?
-        .into_iter()
-        .map(|i| i.into())
-        .collect())
-}
-
 #[tauri::command]
 pub async fn database_get_genres(
     fetcher: tauri::State<'_, DatabaseFetcher>,
 ) -> Result<Vec<String>, TauriChadError> {
-    get_items::<table::ListGenres>(&*fetcher).await
+    fetcher
+        .list_items::<table::ListGenres>()
+        .await
+        .map_err(|e| TauriChadError::from(e))
 }
 
 #[tauri::command]
 pub async fn database_get_languages(
     fetcher: tauri::State<'_, DatabaseFetcher>,
 ) -> Result<Vec<String>, TauriChadError> {
-    get_items::<table::ListLanguages>(&*fetcher).await
+    fetcher
+        .list_items::<table::ListLanguages>()
+        .await
+        .map_err(|e| TauriChadError::from(e))
 }
 
 #[tauri::command]
 pub async fn database_get_tags(
     fetcher: tauri::State<'_, DatabaseFetcher>,
 ) -> Result<Vec<String>, TauriChadError> {
-    get_items::<table::ListTags>(&*fetcher).await
+    fetcher
+        .list_items::<table::ListTags>()
+        .await
+        .map_err(|e| TauriChadError::from(e))
 }
